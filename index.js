9661f660b59de0446faacbe989418f5ff53c46a7
Jordan Choo <jordan.choo@kogneta.com>
2022-06-22 11:12:22 -0400

    Importing NPM pkgs and ENV vars

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Import NPM Pkgs
+require('dotenv').config();
+const axios = require('axios');
+
+// Import ENV vars
+const kgKey = process.env.kgKey || null;
+const acUrl = process.env.acUrl || null
+
 module.exports = {
 
     AutoLookup: async (req, res) => {
