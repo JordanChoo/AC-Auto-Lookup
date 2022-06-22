4d9681d287372d44b4cb9f0f02e36cc18b82818f
Jordan Choo <jordan.choo@kogneta.com>
2022-06-22 11:18:44 -0400

    Added Axios request to AC

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ module.exports = {
         if(req.query.kgKey != kgKey) return res.status(401).send('Not authorized');
         // Check to see if it is a GET Req
         if(req.method != 'GET') return res.status(401).send('Not authorized');
+
+        // Config the API Req
+        var axiosConfig = {
+            method: 'get',
+            url: `${acUrl}/api/3/automations?limit=100`,
+            headers: { 
+              'Api-Token': acApiToken, 
+            }
+        };
+
+        let acAutoResults = await axios.request(axiosConfig);
     }
 
 }
